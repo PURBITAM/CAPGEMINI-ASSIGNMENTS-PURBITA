43519de0c3abb0f7d287416fe742f16ffbc8c7db
PURBITA <purbitamajumdar@gmail.com>
2021-05-13 15:59:50 +0530

    lab 1 exercise 5,6,7,8 added

---
+++ LABBOOK ASSIGNMENTS/corejava8labbookexercises/lab1exercise5.java
@@ -0,0 +1,38 @@
+package corejava8labbookexercises;
+
+import java.util.Scanner;
+
+class sumNumber
+{
+	sumNumber()
+	{
+		
+	}
+	public int calculateSum(int n)
+	{
+		int sum=0;
+		for(int i=1;i<=n;i++)
+		{
+			if(i%3==0 || i%5==0)
+			{
+				sum+=i;
+			}
+		}
+		return sum;
+	}
+}
+
+public class lab1exercise5 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter n : ");
+		int n=sc.nextInt();
+		sumNumber sn=new sumNumber();
+		int sum=sn.calculateSum(n);
+		System.out.println("The sum of first "+n+" natural numbers which are divisible by 3 or 5 : "+sum);
+	}
+
+}
