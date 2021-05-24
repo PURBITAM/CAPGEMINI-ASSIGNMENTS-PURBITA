08280a3c79423eb0cb893c8dc5aec90fc55f9ae7
PURBITA <purbitamajumdar@gmail.com>
2021-05-24 08:34:03 +0530

    submitted

---
+++ LABBOOK ASSIGNMENTS/com/capgemini/lab5/exercise3/com/cg/eis/exception/CheckSalaryMain.java
@@ -0,0 +1,30 @@
+package com.capgemini.lab5.exercise3.com.cg.eis.exception;
+
+import java.util.Scanner;
+
+public class CheckSalaryMain 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Employee Salary : ");
+		double salary=sc.nextDouble();
+		try
+		{
+			if(salary<3000)
+			{
+				throw new EmployeeException("Salary inserted, is below INR 3000.");
+			}
+			else
+			{
+				System.out.println("Salary is : "+salary);
+				System.out.println("Salary is above INR 3000.");
+			}
+		}
+		catch(EmployeeException an)
+		{
+			System.out.println(an.getMessage());
+		}
+	}
+	
+}
