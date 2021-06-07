c205ebf5186d518e070c801914141b675551f45d
PURBITA <purbitamajumdar@gmail.com>
2021-06-07 11:26:44 +0530

    lab submitted

--- LABBOOK ASSIGNMENTS/com/capgemini/lab9/streamapi/exercise5/FactorialMain.java
+++ LABBOOK ASSIGNMENTS/com/capgemini/lab9/streamapi/exercise5/FactorialMain.java
@@ -4,23 +4,20 @@ import java.util.Scanner;
 
 public class FactorialMain {
 
+	public int calculateFactorial(int n) {
+		int result = 1;
+		for(int i=1; i<=n; i++) {
+			result = result * i;
+		}
+		return result;
+	}
 	public static void main(String[] args) {
 		Scanner sm = new Scanner(System.in);
-		FactorialInterface fact = (number)->{
-			int num=1;
-			for(int i=0;i<=number;i++) {
-				if(i==0)
-					num=1;
-				else
-					num=num*i;
-			}
-			return num;
-		};
 		System.out.print("Enter the number : ");
 		int number = sm.nextInt();
-		System.out.println("\nFactorial of "+number+" : "+fact.factorial(number));
-		
-		sm.close();
+		FactorialMain fm=new FactorialMain();
+		FactorialInterface f = fm::calculateFactorial;
+		System.out.println("Factorial of "+number+" : "+f.factorial(number));
 	}
 
 }
